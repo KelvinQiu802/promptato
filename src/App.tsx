a8c5b6597a87567a4e765561dde85384e4b47e88
KelvinQiu802 <kelvinqiu802@outlook.com>
2025-03-25 20:04:16 +0800

    feat: add basic components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
-import { Loader2 } from 'lucide-react'
 import './App.css'
+import Sidebar from '@/components/Sidebar'
+import Editor from '@/components/Editor'
+import OptionPanel from '@/components/OptionPanel'
 
 function App() {
   return (
-    <div className='flex flex-col items-center justify-center h-screen'>
-      <h1 className='text-2xl font-bold text-muted-foreground'>Hello World</h1>
-      <Loader2 className='animate-spin text-muted-foreground mt-2' />
+    <div className='flex'>
+      <Sidebar />
+      <Editor />
+      <OptionPanel />
     </div>
   )
 }
---
+++ src/components/OptionPanel/OptionPanel.tsx
@@ -0,0 +1,7 @@
+export default function OptionPanel() {
+    return (
+        <div className="flex-1/4 h-screen">
+            <h1>OptionPanel</h1>
+        </div>
+    )
+}
